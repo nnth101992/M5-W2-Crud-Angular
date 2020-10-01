14884b1b3821e8368a35b0490904e91e1bbb8a0a
Thinh <nthinh92@gmail.com>
2020-10-01 20:50:58 +0700

    third commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,22 +10,6 @@ const API_URL = 'http://localhost:8080/';
 
 export class ProductService {
 
-  totalId = 4;
-  listProduct: Array<IProduct> = [
-    {
-      id: 1, name: 'Iphone', description: 'Apple'
-    },
-    {
-      id: 2, name: 'Galaxy Fold', description: 'Samsung'
-    },
-    {
-      id: 3, name: 'Reno F', description: 'Oppo'
-    },
-    {
-      id: 4, name: 'P9', description: 'Huawei'
-    }
-  ];
-
   constructor(private http: HttpClient) {
   }
 
@@ -58,16 +42,10 @@ export class ProductService {
   }
 
   createProduct(product: IProduct): void {
-    this.totalId++;
-    product.id = this.totalId;
-    this.listProduct.push(product);
+
   }
 
   deleteProduct(product: IProduct): void {
-    this.listProduct.forEach((item, index) => {
-      if (item === product) {
-        this.listProduct.splice(index, 1);
-      }
-    });
+
   }
 }
